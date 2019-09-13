db1c65c62d93d65459e49f5c284c445f09d1e0e5
jessicakwalters <jessicakwalters@gmail.com>
2019-09-13 07:51:41 -0700

    added tests

--- __tests__/gameModel.test.js
+++ __tests__/gameModel.test.js
@@ -1,12 +1,38 @@
 'use strict';
 
-process.env.SECRET='test';
+const Game = require('../src/gameModel');
 
-describe('',  () => {
+describe('Test the game model',  () => {
 
-  it(' ', () => {
+  let game = new Game();
 
+  it('can instantiate a new game', () => {
+    let testGame = new Game();
+    expect(testGame.text).toBeTruthy();
   });
 
+  it('can generate a new text string.', () => {
+    let initialText = game.text;
+    game.text = game.generateString();
+    function status(){
+      return (initialText === game.text);
+    }
+    expect(status()).toBe(false);
+  });
+
+  it('ccan calculate a winner', () => {
+    let player1 = {
+      name: 'Frank',
+      wordsPerMinute: 58,
+      incorrectEntries: 4,
+    };
+    let player2 = {
+      name: 'Pancakes',
+      wordsPerMinute: 70,
+      incorrectEntries: 4,
+    };
+    game.winner = game.calculateWinner(player1, player2);
+    expect(game.winner.name).toBe('Pancakes');
+  });
 
 });
